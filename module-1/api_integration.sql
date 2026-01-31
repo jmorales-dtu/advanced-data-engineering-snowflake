c9fa311cadc137f6afd4db57eb064ae200d7a12a
Jonathan Morales <jonathan@itzdata.tech>
2026-01-30 19:17:17 -0600

    api integration examples

--- module-1/api_integration.sql
+++ module-1/api_integration.sql
@@ -5,21 +5,21 @@ USE SCHEMA public;
 -- Create credentials
 CREATE OR REPLACE SECRET course_repo.public.github_pat
   TYPE = password
-  USERNAME = ''
+  USERNAME = 'jmorales-dtu'
   PASSWORD = '';
 
 -- Create the API integration
-CREATE OR REPLACE API INTEGRATION
+CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('') -- URL to your GitHub profile
-  ALLOWED_AUTHENTICATION_SECRETS = ()
+  API_ALLOWED_PREFIXES = ('https://github.com/jmorales-dtu') -- URL to your GitHub profile
+  ALLOWED_AUTHENTICATION_SECRETS = (github_pat)
   ENABLED = TRUE;
 
 -- Create the git repository object
 CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
-  API_INTEGRATION =  -- Name of the API integration defined above
-  ORIGIN = '' -- Insert URL of forked repo
-  GIT_CREDENTIALS = ;
+  API_INTEGRATION =  git_api_integration-- Name of the API integration defined above
+  ORIGIN = 'https://github.com/jmorales-dtu/advanced-data-engineering-snowflake.git' -- Insert URL of forked repo
+  GIT_CREDENTIALS = course_repo.public.github_pat;
 
 -- List the git repositories
 SHOW GIT REPOSITORIES;
